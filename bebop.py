d112a31654dfb142b9793ca307c614af1d90370d
martind <martind@mapfactor.com>
2015-02-01 21:16:21 +0100

    collecting some "navdata" from Bebop (d2c_port)

--- bebop.py
+++ bebop.py
@@ -6,11 +6,35 @@
 """
 import sys
 import socket
+import datetime
 
 # hits from https://github.com/ARDroneSDK3/ARSDKBuildUtils/issues/5
 
 HOST = "192.168.42.1"
 DISCOVERY_PORT = 44444
+
+NAVDATA_PORT = 43210 # d2c_port
+COMMAND_PORT = 54321 # c2d_port
+
+class Bebop:
+    def __init__( self ):
+        logFilename = datetime.datetime.now().strftime("navdata_%y%m%d_%H%M%S.bin") # TODO metalog
+        self.navdata = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.navdata.bind( ('',NAVDATA_PORT) )
+#        self.navdata.settimeout( SOCKET_TIMEOUT )
+        self.command = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.logf = open( logFilename, "wb" )
+
+    def update( self, cmd ):
+        "send command and return navdata"
+        data = None
+        while data == None or len(data) == 0:
+            data = self.navdata.recv(4094)
+        self.logf.write(data)
+        self.logf.flush()
+        # TODO send cmd
+        return data
+
 
 def test( outDir ):
     filename = "tmp.bin" # TODO combination outDir + date/time
@@ -25,6 +49,11 @@ def test( outDir ):
             f.write(data)
             f.flush()
             break
+
+    robot = Bebop()
+    for i in xrange(100):
+        robot.update( cmd=None )
+
     f.close()
     s.close()
 
