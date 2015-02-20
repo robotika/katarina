8f9acc8e04077c9df4881eee6f4e22463766d7eb
martind <martind@mapfactor.com>
2015-02-20 16:54:02 +0100

    added option for dumping frames for youtube video "re-packing"

--- video.py
+++ video.py
@@ -2,14 +2,15 @@
 """
   Utility for extraction of video stream from navdata log
   usage:
-       ./video.py <input navdata log> <output video file>
+       ./video.py <input navdata log> <output video file> [<output frames directory>]
 """
 import sys
 import struct
+import os
 from navdata import cutPacket,videoAckRequired
 
 
-def navdata2video( inputFile, outputFile ):
+def navdata2video( inputFile, outputFile, outDir = ".", dumpIndividualFrames=False, startIndex=0 ):
     data = open(inputFile, "rb").read()
     arr = []
     while len(data) > 0:
@@ -19,8 +20,19 @@ def navdata2video( inputFile, outputFile ):
             arr.append( (frameNumber, fragmentNumber, packet[12:]) )
     vf = open(outputFile, "wb")
     prev = None
+    frameIndex = startIndex
+    frame = ""
     for a in sorted(arr):
         if prev != a[:2]:
+            if prev is not None and prev[0] != a[0]:
+                # new frame
+                if dumpIndividualFrames:
+                    fout = open(outDir+os.sep+"frame%04d.bin" % frameIndex, "wb")
+                    fout.write(frame)
+                    fout.close()                    
+                frame = ""
+                frameIndex += 1
+            frame += a[2]
             vf.write(a[2])
         else:
             print "duplicity", prev
@@ -32,7 +44,12 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print __doc__
         sys.exit(2)
-    navdata2video( sys.argv[1], sys.argv[2] )
+    if len(sys.argv) == 3:
+        navdata2video( sys.argv[1], sys.argv[2] )
+    else:
+        outDir = sys.argv[3]
+        startIndex = len(os.listdir( outDir ))
+        navdata2video( sys.argv[1], sys.argv[2], outDir=outDir, dumpIndividualFrames=True, startIndex=startIndex )
 
 # vim: expandtab sw=4 ts=4 
 
