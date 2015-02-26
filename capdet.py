a535f3ffc1fd54de98477f36276a45e9b1503abf
martind <martind@mapfactor.com>
2015-02-26 21:57:14 +0100

    testing cap detection on recorded videos

--- capdet.py
+++ capdet.py
@@ -2,40 +2,27 @@
 """
   The Parrot two color cap detector
   usage:
-       ./capdet.py <input image> [<colors file>]
+       ./capdet.py <colors file> <input image>
 """
 import sys
 import cv2
 import numpy as np
 
-imgResult = None
-imgR,imgG,imgR = None, None, None
 rad = 10
 
-def addColor( col ):
-    maskB = np.logical_and( imgB < col[0]+rad, imgB > col[0]-rad )
-    maskG = np.logical_and( imgG < col[1]+rad, imgG > col[1]-rad )
-    maskR = np.logical_and( imgR < col[2]+rad, imgR > col[2]-rad )
-    mask = np.logical_and(maskR, maskG)
-    mask = np.logical_and(mask, maskB)
-    if col[1] > max(col[0],col[2]):
-        imgResult[mask] = (0,255,0) # green
-    else:
-        imgResult[mask] = (0,128,255) # orange
-
-def onmouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        col = img[y][x]
-        print x,y, col
-        imgResult[y][x]=255
-        addColor( col )
-        cv2.imshow('result', imgResult)
-
-
-def detectTwoColors( img, colorA, colorB ):
-    cv2.imshow('image', img)
-    img2 = cv2.cvtColor( img, cv2.COLOR_RGB2HSV )
-    cv2.imshow('hsv', img2)
+def detectTwoColors( img, capColors ):
+    imgResult = np.zeros( img.shape, np.uint8 )
+    imgB, imgG, imgR = cv2.split(img)
+    for col in capColors:
+        maskB = np.logical_and( imgB < col[0]+rad, imgB > col[0]-rad )
+        maskG = np.logical_and( imgG < col[1]+rad, imgG > col[1]-rad )
+        maskR = np.logical_and( imgR < col[2]+rad, imgR > col[2]-rad )
+        mask = np.logical_and(maskR, maskG)
+        mask = np.logical_and(mask, maskB)
+        if col[1] > max(col[0],col[2]):
+            imgResult[mask] = (0,255,0) # green
+        else:
+            imgResult[mask] = (0,128,255) # orange
 
     gray = cv2.cvtColor( imgResult, cv2.COLOR_BGR2GRAY )
     ret, binary = cv2.threshold( gray, 1, 255, cv2.THRESH_BINARY )
@@ -53,29 +40,60 @@ def detectTwoColors( img, colorA, colorB ):
             cv2.drawContours(maskTmp, [cnt], -1, 255, -1)
             mask = maskTmp > 0
             orange = np.count_nonzero(np.logical_and( mask, cmpRG ))
-            print abs(area), orange, orange/float(abs(area))
-    cv2.imshow('result', imgResult)
+            # print abs(area), orange, orange/float(abs(area))
+    return imgResult
+
+
+def onmouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        col = img[y][x]
+        print col
+        capColors.append( col )        
+        cv2.imshow('result', detectTwoColors(img, capColors))
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print __doc__
         sys.exit(2)
-    filename = sys.argv[1]
-    img = cv2.imread( filename )
-    imgResult = np.zeros( img.shape, np.uint8 )
-    imgB, imgG, imgR = cv2.split(img)
      
-    if len(sys.argv) > 2:
-        for line in open(sys.argv[2]):
-            line = line.split('#')[0]
-            line = line.replace(',',' ').translate(None,"[]")
-            if len(line.split()) == 3:
-                addColor( [int(x) for x in line.split()] )
+    capColors = []
+    for line in open(sys.argv[1]):
+        line = line.split('#')[0]
+        line = line.replace(',',' ').translate(None,"[]")
+        if len(line.split()) == 3:
+            capColors.append( [int(x) for x in line.split()] )
 
-    detectTwoColors( img, None, None )
-    cv2.setMouseCallback("image", onmouse)    
-    cv2.waitKey(0)
+    filename = sys.argv[2]
+
+    # convert navdata to video if necessary
+    from video import navdata2video
+    TMP_VIDEO_FILE = "video.bin"
+    if "navdata" in filename:
+        navdata2video( filename, TMP_VIDEO_FILE )
+        filename = TMP_VIDEO_FILE
+    
+    cap = cv2.VideoCapture( filename )
+    ret, img = cap.read()
+    cv2.imshow('image', img)    
+    cv2.setMouseCallback("image", onmouse)
+    pause = 100
+    if filename.endswith(".jpg"):
+        pause = 0
+    while ret:
+        cv2.imshow('image', img)    
+        cv2.imshow('result', detectTwoColors( img, capColors ))
+        c = cv2.waitKey( pause )
+        if c == 27: # ESC
+            break
+        if c == 8: # Backspace
+            if len(capColors) > 0:
+                capColors = capColors[:-1]
+        elif c == 32:
+            pause = 100 - pause # swap 0 and 100
+        else:
+            ret, img = cap.read()
+
     cv2.destroyAllWindows()
 
 # vim: expandtab sw=4 ts=4 
